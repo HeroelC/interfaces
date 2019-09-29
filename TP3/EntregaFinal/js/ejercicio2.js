ced9572ad362117139c3a0faca9512fd7518d1ca
Heroel <HeroelJCarpinetti@gmail.com>
2019-09-29 14:36:32 -0300

    Agregado menu al juego, pantallas de juego perdido y ganado

--- TP3/EntregaFinal/js/ejercicio2.js
+++ TP3/EntregaFinal/js/ejercicio2.js
@@ -1,14 +1,27 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', () => {
-    let divEjercicio2 = document.getElementById('ejercicio2');
-    
-    divEjercicio2.onclick = (e) => {
-        console.log(randomNumber());
-    }
+    let messiCard = document.getElementById('messi-card');
 
+    messiCard.onclick = (e) => {
+        let option = randomNumber();
+        switch (option) {
+            case 0:
+                messiCard.classList.remove(messiCard.classList.value);
+                messiCard.classList.add('translacion');
+                break;
+            case 1:
+                messiCard.classList.remove(messiCard.classList.value);
+                messiCard.classList.add('escalado');
+                break;
+            case 2:
+                messiCard.classList.remove(messiCard.classList.value);
+                messiCard.classList.add('rotacion');
+                break
+        }
+    }
 });
 
-function randomNumber(){
-    return Math.floor(Math.random() * 10);
+function randomNumber() {
+    return Math.floor(Math.random() * 3);
 }
